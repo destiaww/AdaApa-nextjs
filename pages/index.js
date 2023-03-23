812157e4c8f26012183d62b33bb38bb378a0bd5f
FavianIbra <ibra.yanuarta@gmail.com>
2023-03-23 10:53:35 +0700

    add:footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Navbar from 'ahmad/components/Navbar'
 import { Inter } from '@next/font/google'
+// import Herosection from 'components/Herosection/herosection'
+import Footer from 'components/Footer/footer'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,6 +17,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* <Herosection /> */}
+      <Footer />
       <main>
         
       </main>
