17698efc5869ebce670e855cadf006a10b5bed4a
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-27 13:07:59 +0700

    Add:Carousel

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Footer />
       <main>
-        
+        <section>
+          Hallo
+        </section>
       </main>
+      <Footer />
     </>
 
   )
