3bdb40db8cddb07401df704ff589cf0d69ef0677
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-27 20:28:48 +0700

    Add:FOoter

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,75 +1,104 @@
-import React from 'react';
-import { FaInstagram } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaYoutube } from 'react-icons/fa';
+import styles from 'ahmad/styles/footer.module.css';
+import { FaInstagram, FaTwitter, FaFacebook, FaYoutube } from 'react-icons/fa';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function Footer() {
   return (
-    <div className="bg-gray-50 h-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
-      <div className="p-5">
-        <ul>
-          <p className="text-gray-800 font-bold text-5xl pb-6">
-            Ada<span className="text-blue-600">Apa</span>
-          </p>
-          <p className="md:text-1xl text-center pb-4 ">Kumpulan berita terkini yang lengkap</p>
-          <div className="flex gap-6 pb-5">
-            <FaInstagram className="text-2xl cursor-pointer hover:text-yellow-600" />
-            <FaTwitter className="text-2xl cursor-pointer hover:text-blue-600" />
-            <FaLinkedin className="text-2xl cursor-pointer hover:text-blue-600" />
-            <FaYoutube className="text-2xl cursor-pointer hover:text-red-600" />
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.footer_widget}>
+            <a href="#">
+              <Image src={'./logo.svg'} width={150} height={100} />
+            </a>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos deserunt magni recusandae ut! Natus?</p>
+            <ul className={styles.socials}>
+              <li>
+                <Link href="#">
+                  <FaInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <FaTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <FaFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <FaYoutube />
+                </Link>
+              </li>
+            </ul>
           </div>
-        </ul>
+          <div className={styles.footer_widget}>
+            <h6>Category</h6>
+            <ul className={styles.links}>
+              <li>
+                <Link href={`/`}>Home</Link>
+              </li>
+              <li>
+                <Link href={`/Business`}>Businnes</Link>
+              </li>
+              <li>
+                <Link href={`/politik`}>Politik</Link>
+              </li>
+              <li>
+                <Link href={`/Entertainment`}>Entertainment</Link>
+              </li>
+              <li>
+                <Link href={`/Lifestyle`}>Lifestyle</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.footer_widget}>
+            <h6>Layanan</h6>
+            <ul className={styles.links}>
+              <li>
+                <Link href="#">Forum</Link>
+              </li>
+              <li>
+                <Link href="#">AdaApa Event</Link>
+              </li>
+              <li>
+                <Link href="#">seo optimization</Link>
+              </li>
+              <li>
+                <Link href="#">blog writing</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.footer_widget}>
+            <h6>Help &amp; Support</h6>
+            <ul className={styles.links}>
+              <li>
+                <a href="#">support center</a>
+              </li>
+              <li>
+                <a href="#">live chat</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">terms &amp; conditions</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.copyright_wrapper}>
+          <p>
+            © AdaApa 2023 - All rights reserved
+            <a href="#" target="blank"></a>
+          </p>
+        </div>
       </div>
-      <div className="p-5">
-        <ul>
-          <p className="text-gray-800 font-bold text-2xl pb-4">Kategori</p>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">News</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Edukasi</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Teknologi</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Sport</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Travel</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Food</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Finance</li>
-        </ul>
-      </div>
-      <div className="p-5">
-        <ul>
-          <p className="text-gray-800 font-bold text-2xl pb-4">Layanan</p>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">berbuatbaik.id</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Forum</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">adaapaEvent</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Pasang Mata</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Trans Studio</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Trans Snow World</li>
-        </ul>
-      </div>
-      <div className="p-5">
-        <ul>
-          <p className="text-gray-800 font-bold text-2xl pb-4">Informasi</p>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Redaksi</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Kotak Pos</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Info Iklan</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Privacy Policy</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Pedoman Media Siber</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Disclaimer</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Media Partner</li>
-        </ul>
-      </div>
-      <div className="p-5">
-        <ul>
-          <p className="text-gray-800 font-bold text-2xl pb-4">Jaringan Media</p>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">CNN Indonesia</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">CNBC Indonesia</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Beautynesia</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Insertive</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">CXO Media</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Haibunda</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">Female Daily</li>
-          <li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">SindoNews</li>
-        </ul>
-      </div>
-    </div>
+    </footer>
   );
 }
 
--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -14,6 +14,7 @@ const RootLayout = ({ children, title }) => {
       </Head>
       <Navbar />
       {children}
+      <Footer />
     </>
   );
 };
