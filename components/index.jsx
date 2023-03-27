3776b43320ea4a5eea5c8de5e92c58ee0bbfdfeb
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-27 14:17:52 +0700

    Add:Banner

--- components/index.jsx
+++ components/index.jsx
@@ -1,6 +1,7 @@
-import Highlight from "./Highlight";
-import Carousel from "./Carousel";
-import RootLayout from "./Layout";
-import Feed from "./Feed";
+import Highlight from './Highlight';
+import Carousel from './Carousel';
+import RootLayout from './Layout';
+import Feed from './Feed';
+import Banner from './Banner';
 
-export { RootLayout, Highlight, Carousel, Feed };
+export { RootLayout, Highlight, Carousel, Feed, Banner };
