3773c51717d2c0b116a923836fb2f41411dc623b
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-27 16:52:45 +0700

    Update:Bug

--- pages/detail/[id].jsx
+++ pages/detail/[id].jsx
@@ -9,7 +9,8 @@ import Link from 'next/link';
 
 const Index = ({ data }) => {
   const paragraphs = data.deskripsi.split('\n');
-
+  const filterPopuler = Data.filter((item) => item.populer);
+  const filterComment = Data.filter((item) => item.comment);
   const filterData = Data.filter((item) => item.category === data.category && item.id !== data.id).slice(0, 4);
 
   return (
@@ -38,10 +39,44 @@ const Index = ({ data }) => {
                   <BiShareAlt className={styles.ion} /> 80
                 </div>
               </div>
-              <Image className={styles.img} src={data.foto} width={1150} height={500} />
-              {paragraphs.map((paragraph, index) => (
-                <p key={index}>{paragraph}</p>
-              ))}
+            </div>
+            <div className={styles.wrap}>
+              <div className={styles.content_left}>
+                <Image className={styles.img_head} src={data.foto} width={800} height={500} />
+                {paragraphs.map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+              </div>
+              <div className={styles.content_right}>
+                <div className={styles.wrapper_top}>
+                  <h1 className={styles.head_right}>Berita terpopuler</h1>
+                  {filterPopuler.map((item, index) => (
+                    <Link href={`detail/${item.id}`}>
+                      <div className={styles.news}>
+                        <h1 className={styles.no}>#{index + 1}</h1>
+                        <div className={styles.news_content}>
+                          <h1 className={styles.news_head}>{item.title}</h1>
+                          <p className={styles.news_desc}>AdaApa | {item.time}</p>
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+                <div className={styles.wrapper_bottom}>
+                  <h1 className={styles.head_right}>Komentar Terbanyak</h1>
+                  {filterComment.map((item) => (
+                    <Link href={`detail/${item.id}`}>
+                      <div className={styles.news}>
+                        <Image src={'/comment.svg'} width={24} height={24} />
+                        <div className={styles.news_content}>
+                          <h1 className={styles.news_head}>{item.title}</h1>
+                          <p className={styles.news_desc}>AdaApa | {item.time}</p>
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </section>
