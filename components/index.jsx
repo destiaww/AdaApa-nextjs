58633242911f9c9d0ee9742e43283e562eefeb62
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-27 14:13:39 +0700

    Solve

--- components/index.jsx
+++ components/index.jsx
@@ -1,5 +1,7 @@
-import Highlight from "./Highlight";
-import Carousel from "./Carousel";
-import RootLayout from "./Layout";
+import Highlight from './Highlight';
+import Carousel from './Carousel';
+import RootLayout from './Layout';
+import Feed from './Feed';
+import Carousel from './Carousel';
 
 export { RootLayout, Highlight, Carousel };
